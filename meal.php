8fe0e54eb0dd1c938eb109e39cb19845e2d24f76
Vincekool2 <glassb450m@gmail.com>
2024-07-15 10:25:41 +0200

    mama

---
+++ meal.php
@@ -0,0 +1,25 @@
+<?php
+
+class meal{
+    protected $size;
+    protected $price;
+    protected $base;
+    protected $status;
+    protected $orderedAt;
+
+
+
+    public function pay() {
+        if ($this->status === "en cours de commande"){
+            $this->status = "payé";
+        }
+    }
+
+    public function ship(){
+        if($this -> status === "payé"){
+            $this -> status = "livré";
+        }
+    }
+}
+
+?>
--- partials/header.php
+++ partials/header.php
@@ -21,6 +21,7 @@
 			<li><a href="http://localhost:8888/php/views/products.php">Products</a></li>
 			<li><a href="http://localhost:8888/php/views/connexion.php">Connexion</a></li>
 			<li><a href="http://localhost:8888/php/views/deleteArticle.php">Delete all article</a></li>
+			<li><a href="http://localhost:8888/php/views/pizza.php">pizza</a></li>
 		</ul>
 	</nav>
 
